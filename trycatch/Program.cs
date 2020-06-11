f5b8cb608dab8c58623a70133b22c7838ca6dbb3
amonieks <antoniom.monieks@gmail.com>
2020-06-11 17:43:00 -0300

    trycatch

---
+++ trycatch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Permissao acesso = new Permissao();
+
+
+            acesso.Autorizar();
+        }
+    }
+}
